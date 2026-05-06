6f0c2d778aed59c78bc8bb43b2d14ddac359f5eb
dannielgloria <danniel.gloria@gmail.com>
2026-05-05 19:15:27 -0600

    feat: se actualiza el codigo js y se agregan dos funciones nuevas
    -aplicamos buenas practicas al codigo.
    -Agregamos la funvion para cambiar el estado de habitos.
    -Agregamos la funcion de eliminacion de habito.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 // Variables
 const APP_NAME = "Habit Lab"; // es const porque el valor no cambiará a la ejecución del programa
 
-const STORAGE = "habits-lab:habits"; // se sentraliza en una constante para evitar errores al escribr texto vaias veces
+const STORAGE = "habit-lab:habits"; // se sentraliza en una constante para evitar errores al escribr texto vaias veces
 
 // Estado principal de la aplicacion.
-// Se mantiene en un solo objeto para que sea facil saber que datos estan disponibles en la aplicacion y para evitar tener muchas variables sueltas por el codigo.
+// Se mantiene en un solo objeto para que sea facil saber que datos estan disponibles en la aplicacion y para evitar tener muchas variables 
+// sueltas por el codigo.
 const state = {
-    habits: loadhabits(), // cargamos los hábitos del localStorage al iniciar la aplicación con la función loadhabits()
+    habits: loadHabits(), // cargamos los hábitos del localStorage al iniciar la aplicación con la función loadHabits()
     currentFilter: "all" // estado del filtro actual en este caso mostrara todos los hábitos, pero puede cambiarse a "pending" o "done"
 };
 
@@ -36,23 +37,26 @@ function init() {
 //Registra los eventos principales de la aplicación
 // Esto evitara tener eventoz mezclados con la lógica de la aplicación y hará que el código sea más organizado y fácil de entender.
 function bindEvents() {
-    elements.form.addEventListener("submit", handleFormSubmit); // evento que escucha si oprime el botón de submit del formulario para agregar un nuevo hábito
+    elements.form.addEventListener("submit", handleFormSubmit); // evento que escucha si oprime el botón de submit del formulario para agregar un 
+    // nuevo hábito
 
     elements.filterButtons.forEach((button) => {
-        button.addEventListener("click", handleFilterClick); // evento que escucha si se hace click en alguno de los botones de filtro para cambiar el estado del filtro actual
+        button.addEventListener("click", handleFilterClick); // evento que escucha si se hace click en alguno de los botones de filtro para cambiar 
+        // el estado del filtro actual
     })
 
     // Delegacion de eventos:
     // En lugar de usar un evenyo onclick (es decir que se ejecuta al hacer click en el botón) en el html, 
     // va a escuchar los clics que esten dentro de el contenedor de la lista de hábitos 
     // Esto ayuda a que si se tiene que modicar el html, no se tenga que modificar el código js
-    elements.habitList.addEventListener("click", handleHabitAction); // evento que escucha los clics dentro de la lista de hábitos para manejar las acciones de completar o eliminar un hábito
+    elements.habitList.addEventListener("click", handleHabitAction); // evento que escucha los clics dentro de la lista de hábitos para manejar
+    // las acciones de completar o eliminar un hábito
 }
 
 
 // Carga los hábitos del localStorage
 // se usa un try/catch para evitar que la app falle si el almacenamiento tiene datos invalidos
-function loadhabits() {
+function loadHabits() {
     try {
         const storeHabits = localStorage.getItem(STORAGE); // obtenemos los hábitos almacenados en el localStorage
         
@@ -73,7 +77,7 @@ function saveHabits() {
 }
 
 // funcion para manejar el envio del formulario
-function handleFormSubmit(event) {
+function handleFormSubmit(event) { // handleFormSubmit en español seria: manejarEnvioFormulario
     event.preventDefault(); // evitamos que el formulario se recargue al enviar
 
     const habitName = elements.habitName.value.trim(); // obtenemos el valor del input del nombre del hábito y eliminamos espacios en blanco
@@ -87,11 +91,37 @@ function handleFormSubmit(event) {
     }
 
     // Crear un nuevo hábito
-    addhabit(habitName, habitEnergy); // llamamos a la función para agregar un nuevo hábito con el nombre y energía proporcionados
+    addHabit(habitName, habitEnergy); // llamamos a la función para agregar un nuevo hábito con el nombre y energía proporcionados
 
 
     // Limpiar el formulario después de agregar el hábito
     elements.form.reset(); // reseteamos el formulario para limpiar los campos después de agregar un hábito
     elements.habitName.focus(); // ponemos el foco de nuevo en el input del nombre del hábito para facilitar la entrada de nuevos hábitos
     hideMessage(); // ocultamos cualquier mensaje que se haya mostrado anteriormente
+}
+
+// Cambia el estado de un habito
+// Nos devolvera (retornará) el objeto habito cuando se cambie su estado para poder mostrarse
+function toggleHabit(id) { // toggleHabit en español seria: cambiarEstadoHabito
+    state.habits = state.habits.map((habit) => {
+        if (habit.id !== id) { // buscamos el hábito con el id proporcionado
+            return habit; // si el id no coincide, retornamos el hábito sin cambios
+        }
+
+        return {
+            ...habit, // se devuelve eo habito pero con el estado actualizado
+            done: !habit.done // cambiamos el estado de "done" a su valor contrario (si estaba en true, se cambia a false y viceversa)
+        }; 
+    });
+
+    saveHabits(); // guardamos los hábitos actualizados en el localStorage
+    render(); // renderizamos o actualizamos la lista de hábitos para reflejar el cambio en la interfaz
+}
+
+// Elimina un hábito de la lista
+function deleteHabit(id) {
+    state.habits = state.habits.filter((habit) => habit.id !== id);//filtramos el array de hábitos para eliminar el hábito con el id 
+    // proporcionado y devolvemos un nuevo array sin ese hábito
+    saveHabits();
+    render();
 }
